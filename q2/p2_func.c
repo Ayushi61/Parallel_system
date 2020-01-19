4531dc6d3de7aaf139999d365b0c17ebc7b2ea66
arajend4 <arajend4@ncsu.edu>
2020-01-19 18:43:17 -0500

    plotting1000-sin

--- q2/p2_func.c
+++ q2/p2_func.c
@@ -1,9 +1,8 @@
 #include <math.h>
 
-
 //returns the function y(x) = fn
 double fn(double x)
 {
-          return x*x;
+          return sin(x);
                     //  return x;
                     }
