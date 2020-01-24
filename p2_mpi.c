bc5bbd6e00a756baa71e8a1327cfd5044b14e359
ssudher <ssudher@ncsu.edu>
2020-01-23 22:23:01 -0500

    check output4

--- p2_mpi.c
+++ p2_mpi.c
@@ -187,12 +187,12 @@ int main (int argc, char *argv[])
         if( gat_typ == MAN_G && p2p_typ==NBLK )
         {
             if(rank!=0)
-                //MPI_Isend( &dyc[1], counts[rank], MPI_DOUBLE, 0, 456, MPI_COMM_WORLD, &send_dummy ); /* need to use diff tags*/
-                MPI_Isend( &dyc[1], counts[rank], MPI_DOUBLE, 0, 4567, MPI_COMM_WORLD, &send_dummy ); /* need to use diff tags*/
+                MPI_Isend( &dyc[1], counts[rank], MPI_DOUBLE, 0, 5678, MPI_COMM_WORLD, &send_dummy ); /* need to use diff tags*/
+                //MPI_Isend( &dyc[1], counts[rank], MPI_DOUBLE, 0, i*DYC_TAG, MPI_COMM_WORLD, &send_dummy ); /* need to use diff tags*/
             else
                 for(i=1;i<numproc; i++)
-                    //MPI_Irecv( fdyc+disp[i], counts[i], MPI_DOUBLE, i, 456, MPI_COMM_WORLD, &gather_data[(3*(i-1))+2] );
-                    MPI_Irecv( fdyc+disp[i], counts[i], MPI_DOUBLE, i, 4567, MPI_COMM_WORLD, &gather_data[(3*(i-1))+2] );
+                    MPI_Irecv( fdyc+disp[i], counts[i], MPI_DOUBLE, i, 5678, MPI_COMM_WORLD, &gather_data[(3*(i-1))+2] );
+                    //MPI_Irecv( fdyc+disp[i], counts[i], MPI_DOUBLE, i, i*DYC_TAG, MPI_COMM_WORLD, &gather_data[(3*(i-1))+2] );
         }
 
         /* need to setup non-bloaking manual  */
