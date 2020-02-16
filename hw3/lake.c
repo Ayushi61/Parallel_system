dfbf3996bbf67a95a85df657982829c3e328eb30
arajend4 <arajend4@ncsu.edu>
2020-02-16 05:05:52 -0500

    v1 inner loop

--- hw3/lake.c
+++ hw3/lake.c
@@ -218,6 +218,7 @@ void run_sim(double *u, double *u0, double *u1, double *pebbles, int n, double h
      //double t;
     for( i = 0; i < n; i++)
     {
+      #pragma omp parallel for private(j,idx) num_threads(nthreads)
       for( j = 0; j < n; j++)
       {
         idx = j + i * n;
