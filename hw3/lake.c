1a5fea271c3dfe611cb3201579728bdc1ba5b926
arajend4 <arajend4@ncsu.edu>
2020-02-16 05:25:33 -0500

    v0 collapse

--- hw3/lake.c
+++ hw3/lake.c
@@ -216,8 +216,8 @@ void run_sim(double *u, double *u0, double *u1, double *pebbles, int n, double h
     /* run a central finite differencing scheme to solve
  *      * the wave equation in 2D */
      //double t;
-    #pragma omp parallel for collapse(2) private(i,j,idx) num_threads(nthreads)
-    //#pragma omp parallel for private(i,j,idx) num_threads(nthreads)
+    //#pragma omp parallel for collapse(2) private(i,j,idx) num_threads(nthreads)
+    #pragma omp parallel for private(i,j,idx) num_threads(nthreads)
     for( i = 0; i < n; i++)
     {
      // #pragma omp parallel for private(j,idx) num_threads(nthreads)
