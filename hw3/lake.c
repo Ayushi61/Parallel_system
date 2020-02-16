257fad22f1bf988d8f1afc79f7d798f619e5a408
arajend4 <arajend4@ncsu.edu>
2020-02-16 04:04:34 -0500

    v0 check

--- hw3/lake.c
+++ hw3/lake.c
@@ -239,8 +239,8 @@ void run_sim(double *u, double *u0, double *u1, double *pebbles, int n, double h
     }
 
     /* update the calculation arrays for the next time step */    
-    //memcpy(uo, uc, sizeof(double) * n * n);
-    //memcpy(uc, un, sizeof(double) * n * n);
+    memcpy(uo, uc, sizeof(double) * n * n);
+    memcpy(uc, un, sizeof(double) * n * n);
     //temp=u0;
     //u0=uc;
     //uc=un;
