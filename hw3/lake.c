38052287c54b2c0a1650fc7a7a803ef312bda18f
arajend4 <arajend4@ncsu.edu>
2020-02-20 16:37:27 -0500

    optimum with while acc p3 final opti

--- hw3/lake.c
+++ hw3/lake.c
@@ -224,11 +224,12 @@ void run_sim(double *u, double *u0, double *u1, double *pebbles, int n, double h
    * be aware the possibility exists for madness and mayhem */
   dt = h / 2.;
   /* loop until time >= end_time */
+
+  
   #ifdef _OPENACC
   printf("openacc");
   #pragma acc data copy(un[:n*n],uc[:n*n],uo[:n*n],pebbles[:n*n])
-  
-  for(t=0;t<=end_time;t+=dt)
+  while(1)
   {
 
     /* run a central finite differencing scheme to solve
@@ -268,6 +269,7 @@ void run_sim(double *u, double *u0, double *u1, double *pebbles, int n, double h
 	uc[i]=un[i];		
     }    
     /* have we reached the end? */
+    if(!tpdt(&t,dt,end_time)) break;
   }
   #endif
   #ifdef _OPENMP
